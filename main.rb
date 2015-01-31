df29282d488f8a800a3c0060c221ec2cc608fcd7
rfelixallen <rfelixallen@gmail.com>
2015-01-31 12:59:08 -0500

    started basic character creation. currently the crete player method isnt working

--- main.rb
+++ main.rb
@@ -6,6 +6,21 @@ puts "Hello world!"
 test_globe
 test_people
 
-puts "What is your name? \n"
-player = gets.chomp.downcase
-puts player
+puts "Lets make you a character. First, what is your name?\n"
+name1 = gets.chomp
+puts name1
+puts "Next, what is your gender? Type Male or Female.\n"
+gender1 = gets.chomp
+puts gender1
+puts "Now set your starting city. This will also choose your faith. \n"
+puts "Your choises are Venice or Baghdad. \n"
+origin_city1 = gets.chomp
+if origin_city1 == 'Venice'
+	religion1 = 'Christian'
+else
+	religion1 = 'Muslim'
+end
+puts origin_city1
+puts religion1
+player1 = Player.create_player(name1, gender1, religion1, origin_city1)
+player1.biography
