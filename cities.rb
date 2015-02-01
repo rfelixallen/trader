42e5363132c73306d2027dfdd1d2f586d93b6bf8
rfelixallen <rfelixallen@gmail.com>
2015-01-31 20:41:53 -0500

    cleaned up code

--- cities.rb
+++ cities.rb
@@ -8,21 +8,3 @@ class City
 		@region_location = region_location
 	end
 end
-
-#populate regions with cities
-=begin
-regions1 = globe.regions
-regions1 = ["Europe", "Mid East"]
-cities1 = ["Rome", "Venice"]
-cities2 = ["Baghdad", "Mecca"]
-
-#Make a hash where each array entry becomes the key in a new hash, and the city arrays are the value.
-
-# By Hand
-c = Hash.new
-c['Europe'] = cities1
-c['Mid East'] = cities2
-
-c['Europe'] << "Paris"  # This will add Paris to the hash.
-	
-end
