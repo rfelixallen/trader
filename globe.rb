b307e5b311576898f36353a2667977b346f3538a
rfelixallen <rfelixallen@gmail.com>
2015-01-31 19:13:34 -0500

    adding subclasses for region and city

--- globe.rb
+++ globe.rb
@@ -1,3 +1,22 @@
 def test_globe
 	puts 'globe.rb has loaded!'
 end
+
+Class Globe
+	attr_accessor :terra, :regions, :cities
+	@@terra = ['Earth']
+	@@regions = ['Europe', 'Middle East']
+	@@cities = ['Venice', 'Baghdad']
+
+	def initialize
+		@terra = terra
+		@regions = regions
+		@cities = cities
+	end
+end
+
+Class Region
+end
+
+Class City
+end
