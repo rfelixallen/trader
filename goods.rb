3a93456a2c4362d109a96cb0c677a020005b6f7b
rfelixallen <rfelixallen@gmail.com>
2015-02-02 11:54:56 -0500

    added some sample goods as a hash. working on buying/selling

--- goods.rb
+++ goods.rb
@@ -5,9 +5,16 @@ end
 class Goods
 	attr_accessor :title, :type, :base_value
 
+	inventory = {"Wheat" => 1, "Swords" => 5, "Jewlery" => 10}
+
 	def initialize(title, type, base_value)
 		@title = title
 		@type = type
 		@base_value = base_value
+	end
+
+	def for_sale
+		puts "Today the market has the following: \n"
+		inventory.each { |inventory| puts inventory + "\n"}
 	end
 end
