63112f66088375fb35c451bc915689880c2fe896
Robert Allen <rfelixallen@gmail.com>
2015-02-09 21:56:57 -0500

    Still kind of janky, but it actually mostly works. Its just doesnt have error checking and the UI is unclear

--- basic.rb
+++ basic.rb
@@ -1,14 +1,9 @@
 # This is to test a basic trading mechanicsm.
-
-puts "Hello!\n"
+puts "Welcome to Basic Trading!\n"
+puts "This is an exciting game where you buy and sell only two items in two locations.\n"
 
 def instructions
-	puts "Type the following commands.\n"
-	puts "I == See Inventory\n"
-	puts "B == Buy Goods\n"
-	puts "S == Sell Goods\n"
-	puts "T == Travel to other market\n"
-	puts "Q == Quit game\n"
+puts "Type (I)nventory, (B)uy, (S)ell, (T)ravel or (Q)uit.\n"
 end
 
 def inventory(money, item_a_count, item_b_count)
@@ -16,6 +11,7 @@ def inventory(money, item_a_count, item_b_count)
 	puts "Money: $#{money}.\n"
 	puts "Item A: #{item_a_count}\n"
 	puts "Item B: #{item_b_count}\n"
+	puts "Hit Enter to continue.\n"
 end
 
 def list_goods(item_a, item_b)
@@ -32,7 +28,6 @@ def for_sale
 		item_b = item_b*1.5
 	end
 end
-puts "Methods defined.\n"
 
 money = 100.0
 location = 0
@@ -40,14 +35,11 @@ item_a = 10.0
 item_a_count = 0
 item_b = 10.0
 item_b_count = 0
-puts "Variables declared\n"
 
-puts "Beginning loop.\n"
-puts "Type (I)nventory, (B)uy, (S)ell, (T)ravel or (Q)uit.\n"
+puts "Hit Enter to Start.\n"
 while command = gets.chomp
 	puts "Start of loop.\n"
-	instructions
-	inventory(money, item_a_count, item_b_count)
+	#instructions
 	if location == 0
 		puts "You are in Location A.\n"
 	else
@@ -68,45 +60,49 @@ while command = gets.chomp
 			cost = units*item_a
 			money = money - cost
 			item_a_count = item_a_count + units
-			puts "You bought #{units} of Item A for #{cost}."
+			puts "You bought #{units} of Item A for $#{cost}.\n"
+			puts "Hit Enter to continue.\n"
 		elsif buy == "B"
 			puts "How many units?\n"
 			units = gets.chomp.to_f
 			cost = units*item_b
 			money = money - cost
 			item_b_count = item_b_count + units
-			puts "You bought #{units} of Item B for #{cost}."
+			puts "You bought #{units} of Item B for $#{cost}.\n"
+			puts "Hit Enter to continue.\n"
 		elsif buy == "X"
 			puts "Cancel trade.\n"
 			break
 		else
-			puts "Type A, B or X to cancel.\n"
+			instructions
 		end
 	when "S"
 		puts "You want to Sell.\n"
 		list_goods(item_a, item_b)
 		puts "Type which good you want to sell: Item A (A) or Item B (B)\n"
-		buy = gets.chomp
+		sell = gets.chomp
 		puts "How many do you want to sell?\n"
 		if sell == "A"
 			puts "How many units?\n"
-			units = gets.chomp
+			units = gets.chomp.to_f
 			cost = units*item_a
 			money = money + cost
 			item_a_count = item_a_count - units
-			puts "You sold #{units} of Item A for #{cost}."
+			puts "You sold #{units} of Item A for $#{cost}.\n"
+			puts "Hit Enter to continue.\n"
 		elsif sell == "B"
 			puts "How many units?\n"
-			units = gets.chomp
+			units = gets.chomp.to_f
 			cost = units*item_b
 			money = money + cost
 			item_b_count = item_b_count - units
-			puts "You sold #{units} of Item B for #{cost}."
+			puts "You sold #{units} of Item B for $#{cost}.\n"
+			puts "Hit Enter to continue.\n"
 		elsif sell == "X"
 			puts "Cancel trade.\n"
 			break
 		else
-			puts "Type A, B or X to cancel.\n"
+			instructions
 		end
 	when "T"
 		puts "You are traveling to the other market.\n"
@@ -115,10 +111,11 @@ while command = gets.chomp
 		else
 			location = 0
 		end
+		puts "You are traveling."
 	when "Q"
 		puts "Goodbye!\n"
 		break
 	else
-		puts "Enter B, S, T or Q.\n"
+		instructions
 	end
 end
